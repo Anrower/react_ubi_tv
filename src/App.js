1b46a2d0b581fd3fad9f6e3753f03c12516a5a71
Denis Shneider <anrowerdd@gmail.com>
2022-01-19 23:14:40 +0300

    feat: add function and class components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/counter';
 
 function App() {
   //  Использовал деструктуризацию так как функция,
   //  UseState возвращает массив из двух объектов
   //  1ый аргумент функции.
   //  2ой Функция которая обновляет State
-  const [likes, setLikes] = useState(5)
   const [value, setValue] = useState('Input text')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h2>{value}</h2>
+      <Counter />
+      <ClassCounter />
       {/* Применял двухсторонее связывание.
        Состояния h2 и Input  */}
-      <input
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
