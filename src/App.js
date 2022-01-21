320ea711f877a6fa1627b84739dfd1f4b6559962
Denis Shneider <anrowerdd@gmail.com>
2022-01-21 17:24:22 +0300

    feat: add dynamic props transfer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/counter';
 import PostItem from './components/PostItem';
 import './styles/App.css'
 
 function App() {
-  //  Использовал деструктуризацию так как функция,
-  //  UseState возвращает массив из двух объектов
-  //  1ый аргумент функции.
-  //  2ой Функция которая обновляет State
-  const [value, setValue] = useState('Input text')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'PHP', body: 'Description' },
+    { id: 3, title: 'Goland', body: 'Description' },
+  ])
 
   return (
     <div className="App">
-      <PostItem />
+      {posts.map(post =>
+        <PostItem post={post} />
+      )}
     </div>
   );
 }
