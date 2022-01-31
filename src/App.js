fe7537e3000a9468358d5de36e8c35b2a357d650
Denis Shneider <anrowerdd@gmail.com>
2022-01-31 22:08:16 +0300

    feat: add PopUp component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PostList from './components/PostList';
 import './styles/App.css'
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
     { id: 3, title: 'Goland', body: 'OI ' },
   ])
   const [filter, setFilter] = useState({ sort: '', query: '' })
-
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -27,6 +29,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   // get post from child compontent
@@ -36,21 +39,23 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
+        Create Post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: '15px 0 ' }} />
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
-      {sortedAndSearchedPosts.length
-        ?
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Post list №1' />
-        :
-        <h2 style={{ textAlign: 'center' }}>
-          Posts not found!
-        </h2>
-      }
-    </div>
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title='Post list №1'
+      />
+    </div >
   );
 }
 
