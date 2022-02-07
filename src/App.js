a15a84c54c659e18bf88ddb01001917802352740
Denis Shneider <anrowerdd@gmail.com>
2022-02-07 20:04:17 +0300

    feat: add utils folder and implement pagination func

--- src/App.js
+++ src/App.js
@@ -9,18 +9,27 @@ import { usePosts } from './hooks/UsePosts'
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 
 function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  let pagesArray = getPagesArray(totalPages);
+
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
   })
+
 
   useEffect(() => {
     fetchPosts()
@@ -34,6 +43,11 @@ function App() {
   // get post from child compontent
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts()
   }
 
   return (
@@ -53,13 +67,27 @@ function App() {
         <h1>Error ${postError}</h1>
       }
       {isPostsLoading
-        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
-        : <PostList
+        ?
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
+        :
+        <PostList
           remove={removePost}
           posts={sortedAndSearchedPosts}
-          title='Post list №1' />
+          title='Post list №1'
+        />
       }
-    </div >
+      <div className='page__wrapper'>
+        {pagesArray.map(p =>
+          <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'}
+          >
+            {p}
+          </span>
+        )}
+      </div>
+    </div>
   );
 }
 
